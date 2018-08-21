a297132b38f1eb9db1e8e667d839e51086c646ca
schulzy <schulzy@freemail.hu>
2018-08-21 15:00:24 +0200

    Create payload for latest version

---
+++ Schulzy.FoEBot.BL/Version1133/Payload.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Schulzy.FoEBot.Interface;
+
+namespace Schulzy.FoEBot.BL.Version1133
+{
+    class Payload : IPayload
+    {
+        public string GetPayload(PayloadData data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
